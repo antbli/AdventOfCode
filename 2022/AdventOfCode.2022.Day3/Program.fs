9b002cc7bdc5f6b0b2b6dd8631de4eb9fe9bc00a
anton <anton.blixter@gmail..com>
2022-12-04 21:21:52 +0100

    Add day 3 solution

--- 2022/AdventOfCode.2022.Day3/Program.fs
+++ 2022/AdventOfCode.2022.Day3/Program.fs
@@ -1,7 +1,40 @@
-﻿open System.IO
+﻿open System
+open System.IO
 
 let input =
     File.ReadAllLines(Path.Join(__SOURCE_DIRECTORY__, "input.txt"))
 
-// printfn $"Part 1 result: {result}"
-// printfn $"Part 2 result: {result}"
+let rec findCommonItem (rucksacks: char array array) : char =
+    let filteredHead =
+        rucksacks[0]
+        |> Array.filter (fun item -> rucksacks[1] |> Array.contains item)
+        |> Array.distinct
+
+    if filteredHead.Length = 1 then
+        filteredHead[0]
+    else
+        findCommonItem (Array.append [| filteredHead |] (rucksacks |> Array.removeManyAt 0 2))
+
+let getItemPriority (item: char) : int =
+    if Char.IsUpper(item) then
+        int item - 38
+    else
+        int (Char.ToUpper(item)) - 64
+
+let part1Result =
+    input
+    |> Array.map (fun rucksack -> rucksack.ToCharArray() |> Array.splitInto 2)
+    |> Array.map findCommonItem
+    |> Array.map getItemPriority
+    |> Array.sum
+
+let part2Result =
+    input
+    |> Array.map (fun rucksack -> rucksack.ToCharArray())
+    |> Array.splitInto (input.Length / 3)
+    |> Array.map findCommonItem
+    |> Array.map getItemPriority
+    |> Array.sum
+
+printfn $"Part 1 result: {part1Result}"
+printfn $"Part 2 result: {part2Result}"
