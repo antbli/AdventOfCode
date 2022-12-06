2326dbd600490c3f273d63e2dd833264c2ab8be9
anton <anton.blixter@gmail..com>
2022-12-06 06:31:24 +0100

    Add day 6 solution

---
+++ 2022/AdventOfCode.2022.Day6/Program.fs
@@ -0,0 +1,19 @@
+﻿open System.IO
+
+let input =
+    File.ReadAllText(Path.Join(__SOURCE_DIRECTORY__, "input.txt"))
+let rec findMarkerPos (maybeMarkerPos: int) (charLength: int) : int =
+    let maybeMarker = input.Substring(maybeMarkerPos - charLength, charLength)
+    
+    let uniqueCharLength =
+        maybeMarker.ToCharArray()
+        |> Array.distinct
+        |> Array.length
+    
+    if uniqueCharLength = charLength then
+        maybeMarkerPos
+    else
+        findMarkerPos (maybeMarkerPos + 1) charLength
+
+printfn $"Part 1 result: {findMarkerPos 4 4}"
+printfn $"Part 2 result: {findMarkerPos 14 14}"
