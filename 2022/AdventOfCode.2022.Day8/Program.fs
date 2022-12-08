1677fc754d4b1e00e6c6c8cb08ac7c7f8646d4e5
Anton Blixter <anton.blixter@antura.se>
2022-12-08 17:41:13 +0100

    Add day 8 part 2 solution

--- 2022/AdventOfCode.2022.Day8/Program.fs
+++ 2022/AdventOfCode.2022.Day8/Program.fs
@@ -13,6 +13,19 @@ let treeGrid =
 let transposedTreeGrid =
     treeGrid |> Array.transpose
 
+let getSurroundingTrees ((xIndex, yIndex): int * int) : int array * int array * int array * int array =
+    let frontX, backX =
+        treeGrid[yIndex] |> Array.splitAt xIndex
+
+    let backX = backX |> Array.tail
+
+    let frontY, backY =
+        transposedTreeGrid[xIndex] |> Array.splitAt yIndex
+
+    let backY = backY |> Array.tail
+
+    frontX, backX, frontY, backY
+
 let isOuterTree ((xIndex, yIndex): int * int) : bool =
     xIndex = 0
     || yIndex = 0
@@ -30,15 +43,8 @@ let isVisible ((xIndex, yIndex): int * int) : bool =
     else
         let tree = treeGrid[yIndex][xIndex]
 
-        let frontX, backX =
-            treeGrid[yIndex] |> Array.splitAt xIndex
-
-        let backX = backX |> Array.tail
-
-        let frontY, backY =
-            transposedTreeGrid[xIndex] |> Array.splitAt yIndex
-
-        let backY = backY |> Array.tail
+        let frontX, backX, frontY, backY =
+            getSurroundingTrees (xIndex, yIndex)
 
         let isVisibleFromLeft =
             isVisibleWithTreesInFront frontX tree
@@ -57,6 +63,41 @@ let isVisible ((xIndex, yIndex): int * int) : bool =
         || isVisibleFromTop
         || isVisibleFromBottom
 
+let getVisibleTreesInFront (treesInFront: int array) (tree: int) : int =
+    match treesInFront with
+    | [||] -> 0
+    | treesInFront ->
+        let visibleTreesInFrontIndex =
+            treesInFront
+            |> Array.tryFindIndex (fun treeInFront -> treeInFront >= tree)
+
+        match visibleTreesInFrontIndex with
+        | None -> treesInFront.Length
+        | Some index -> index + 1
+
+let getScenicScore ((xIndex, yIndex): int * int) : int =
+    let tree = treeGrid[yIndex][xIndex]
+
+    let frontX, backX, frontY, backY =
+        getSurroundingTrees (xIndex, yIndex)
+
+    let visibleTreesToLeft =
+        getVisibleTreesInFront (frontX |> Array.rev) tree
+
+    let visibleTreesToRight =
+        getVisibleTreesInFront backX tree
+
+    let visibleTreesToTop =
+        getVisibleTreesInFront (frontY |> Array.rev) tree
+
+    let visibleTreesToBottom =
+        getVisibleTreesInFront backY tree
+
+    visibleTreesToLeft
+    * visibleTreesToRight
+    * visibleTreesToTop
+    * visibleTreesToBottom
+
 let part1Result =
     treeGrid
     |> Array.mapi (fun yIndex xTrees ->
@@ -66,5 +107,13 @@ let part1Result =
     |> Array.filter id
     |> Array.length
 
+let part2Result =
+    treeGrid
+    |> Array.mapi (fun yIndex xTrees ->
+        xTrees
+        |> Array.mapi (fun xIndex _ -> getScenicScore (xIndex, yIndex)))
+    |> Array.concat
+    |> Array.max
+
 printfn $"Part 1 result: {part1Result}"
-// printfn $"Part 2 result: {part2Result}"
+printfn $"Part 2 result: {part2Result}"
