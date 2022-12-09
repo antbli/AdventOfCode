b69579d5b716da12e604ce14afd78cae73124d3e
anton <anton.blixter@gmail..com>
2022-12-09 06:03:58 +0100

    Prepare day 9

---
+++ 2022/AdventOfCode.2022.Day9/Program.fs
@@ -0,0 +1,9 @@
+﻿open System.IO
+
+let input =
+    File.ReadAllLines(Path.Join(__SOURCE_DIRECTORY__, "input.txt"))
+
+
+
+// printfn $"Part 1 result: {part1Result}"
+// printfn $"Part 2 result: {part2Result}"
