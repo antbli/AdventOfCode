ff00142ef132217d48bee68046a0840656146796
anton <anton.blixter@gmail..com>
2022-12-12 22:23:12 +0100

    Prepare day 12

---
+++ 2022/AdventOfCode.2022.Day12/Program.fs
@@ -0,0 +1,9 @@
+﻿open System.IO
+
+let input =
+    File.ReadAllLines(Path.Join(__SOURCE_DIRECTORY__, "input1.txt"))
+
+
+
+// printfn $"Part 1 result: {part1Result[0] * part1Result[1]}"
+// printfn $"Part 2 result: {part2Result[0] * part2Result[1]}"
