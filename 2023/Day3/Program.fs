c6341b4ae8e2f497175fab875e0ac79dc0baec11
Anton Blixter <anton.blixter@antura.se>
2023-12-02 06:53:29 +0100

    Prepare day 3

---
+++ 2023/Day3/Program.fs
@@ -0,0 +1,11 @@
+﻿open System.IO
+
+let input = File.ReadAllLines(Path.Join(__SOURCE_DIRECTORY__, "input.txt"))
+
+let part1Result = 0
+
+printfn $"Part 1 result: {part1Result}"
+
+let part2Result = 0
+
+printfn $"Part 2 result: {part2Result}"
