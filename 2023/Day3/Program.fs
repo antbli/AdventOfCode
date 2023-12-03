1c93d00a13abefdb1a2a8e43c03b54337c0808ee
Anton Blixter <anton.blixter@antura.se>
2023-12-03 19:46:22 +0100

    Refactor

--- 2023/Day3/Program.fs
+++ 2023/Day3/Program.fs
@@ -1,57 +1,78 @@
 ﻿open System
 open System.IO
 
+type Symbol =
+    { Value: char
+      RowIndex: int
+      ColumnIndex: int }
+
+type NumberString =
+    { Value: string
+      RowIndex: int
+      ColumnIndex: int
+      AdjacentSymbol: Symbol option }
+
 let input = File.ReadAllLines(Path.Join(__SOURCE_DIRECTORY__, "input.txt"))
 
 let grid = input |> Array.map Seq.toArray
 
-let toNumberStringsWithRowAndColumnIndex (rowIndex: int) (row: char array) : (string * int * int) list =
+let toNumberStrings (rowIndex: int) (row: char array) : NumberString list =
     row
     |> Array.indexed
     |> Array.filter (fun (_, char) -> Char.IsDigit(char))
     |> Array.fold
-        (fun numberStringsWithRowAndColumnIndex (columnIndex, digitChar) ->
-            match numberStringsWithRowAndColumnIndex |> List.rev with
-            | [] -> List.singleton (string digitChar, rowIndex, columnIndex)
-            | (previousNumberString, rowIndex, previousNumberStringColumnIndex) :: tail ->
+        (fun numberStrings (columnIndex, digitChar) ->
+            match numberStrings |> List.rev with
+            | [] ->
+                [ { Value = string digitChar
+                    RowIndex = rowIndex
+                    ColumnIndex = columnIndex
+                    AdjacentSymbol = None } ]
+            | previousNumberString :: tail ->
                 let shouldAppendDigit =
-                    previousNumberString.Length + previousNumberStringColumnIndex = columnIndex
+                    previousNumberString.Value.Length + previousNumberString.ColumnIndex = columnIndex
 
                 if shouldAppendDigit then
-                    List.append
-                        tail
-                        [ previousNumberString + string digitChar, rowIndex, previousNumberStringColumnIndex ]
+                    [ { previousNumberString with
+                          Value = previousNumberString.Value + string digitChar } ]
+                    |> List.append tail
                 else
-                    List.append numberStringsWithRowAndColumnIndex [ string digitChar, rowIndex, columnIndex ])
-        List.empty<string * int * int>
+                    [ { Value = string digitChar
+                        RowIndex = rowIndex
+                        ColumnIndex = columnIndex
+                        AdjacentSymbol = None } ]
+                    |> List.append numberStrings)
+        List.empty
 
-let filterValidIndices ((rowIndex, columnIndex): int * int) : bool =
+let areValidIndices ((rowIndex, columnIndex): int * int) : bool =
     rowIndex >= 0
     && rowIndex < grid.Length
     && columnIndex >= 0
     && columnIndex < grid[0].Length
 
-let filterNumbersByAdjacentSymbol ((numberString, rowIndex, columnIndex): string * int * int) : bool =
-    let firstColumnIndexToCheck = columnIndex - 1
-    let lastColumnIndexToCheck = numberString.Length + columnIndex
+let hasAdjacentSymbol (numberString: NumberString) : bool =
+    let firstColumnIndexToCheck = numberString.ColumnIndex - 1
+    let lastColumnIndexToCheck = numberString.Value.Length + numberString.ColumnIndex
 
     [ firstColumnIndexToCheck..lastColumnIndexToCheck ]
-    |> List.map (fun columnIndex -> [ rowIndex - 1, columnIndex ] |> List.append [ rowIndex + 1, columnIndex ])
+    |> List.map (fun columnIndex ->
+        [ numberString.RowIndex - 1, columnIndex ]
+        |> List.append [ numberString.RowIndex + 1, columnIndex ])
     |> List.collect id
-    |> List.append [ rowIndex, firstColumnIndexToCheck ]
-    |> List.append [ rowIndex, lastColumnIndexToCheck ]
-    |> List.filter filterValidIndices
+    |> List.append [ numberString.RowIndex, firstColumnIndexToCheck ]
+    |> List.append [ numberString.RowIndex, lastColumnIndexToCheck ]
+    |> List.filter areValidIndices
     |> List.exists (fun (rowIndex, columnIndex) ->
         let char = grid[rowIndex][columnIndex]
         not (Char.IsDigit(char)) && char <> '.')
 
 let part1Result =
     grid
-    |> Array.mapi toNumberStringsWithRowAndColumnIndex
+    |> Array.mapi toNumberStrings
     |> Array.map List.toArray
     |> Array.collect id
-    |> Array.filter filterNumbersByAdjacentSymbol
-    |> Array.map (fun (numberString, _, _) -> int numberString)
+    |> Array.filter hasAdjacentSymbol
+    |> Array.map (fun numberString -> int numberString.Value)
     |> Array.sum
 
 printfn $"Part 1 result: {part1Result}"
